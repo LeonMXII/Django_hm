ed707cb4c4701582585e203fd2c37d92cea12a2c
LeonMXII <159937264+LeonMXII@users.noreply.github.com>
2024-11-20 19:21:47 +0300

    Update views.py

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,15 +16,37 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
+    'shava': {
+        'лаваш, шт': 1,
+        'курица, г': 0.3,
+        'огурец, г': 0.070,
+        'помидор, г': 0.075,
+        'лук, г': 0.05,
+        'соус, г': 0.1,
+    },
+
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+def recipe_all(request, dish):
+    servings = request.GET.get('servings', 1)
+
+    try:
+        servings = int(servings)
+        if servings <= 0:
+            raise ValueError("Ушли в минус:)")
+    except ValueError:
+        return render(request, 'calculator/index.html', {'recipe': None})
+
+
+    recipe = DATA.get(dish)
+
+    if not recipe:
+        return render(request, 'calculator/index.html', {'recipe': None})
+
+    scaled_recipe = {ingredient: quantity * servings for ingredient, quantity in recipe.items()}
+
+    context = {
+        'recipe': scaled_recipe
+    }
+
+    return render(request, 'calculator/index.html', context)
